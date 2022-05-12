d00288e34237052f7693c15d8813c07142e19a0b
javi <narutojavibanda@gmail.com>
2022-05-12 21:19:07 +0200

    Create Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
